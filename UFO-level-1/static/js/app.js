7e9b3afbb8ffabc6c98d9321350174132fe7723b
Kimberly Flores <kimberlyflores@Kimberlys-MBP.localdomain>
2020-11-09 22:49:38 -0600

    adding in the code for the table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+
 // YOUR CODE HERE!
+tableData.forEach(items => {
+    var row = tbody.append("tr");
+    Object.entries(items).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+
+    });
+
+
+});
+
+
