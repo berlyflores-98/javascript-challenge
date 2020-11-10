581e6b949b6cee73d2886e5ac106143574230bbf
Kimberly Flores <kimberlyflores@Kimberlys-MBP.localdomain>
2020-11-09 23:38:46 -0600

    adding in the filter selection based on date

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,18 +1,39 @@
 // from data.js
 var tableData = data;
-
+//starting the table
 var tbody = d3.select("tbody");
 
+// Getting a reference to the button on the page with the id property set to `click-me`
+var button = d3.select("#filter-btn");
+
+button.on("click", handleClick);
+
 // YOUR CODE HERE!
-tableData.forEach(items => {
-    var row = tbody.append("tr");
+function handleClick(){
+      // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    console.log(inputElement);
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    //looping through the table to filter on the selected date
+    tableData.forEach(items => {
+        //creating rows
+        var row = tbody.append("tr");
+    //looping through each entry
     Object.entries(items).forEach(([key, value]) => {
+        //selecting only the ones that have the date selected by user
+        if (inputValue === items.datetime) {
+        //putting in the information based on selection
         var cell = row.append("td");
         cell.text(value);
+        }
 
     });
 
 
 });
+
+};
 
 
